74d52dfce3d9f9ef18f6afb6802d8ce76d6840a6
Kawan Amelung <kawan.amelung@ise.fraunhofer.de>
2024-03-08 16:22:11 +0100

    keymap changes for mini.surround

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,40 +1,49 @@
 return {
-  { -- Collection of various small independent plugins/modules
-    'echasnovski/mini.nvim',
-    config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [']quote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
+	{ -- Collection of various small independent plugins/modules
+		"echasnovski/mini.nvim",
+		config = function()
+			-- Better Around/Inside textobjects
+			--
+			-- Examples:
+			--  - va)  - [V]isually select [A]round [)]paren
+			--  - yinq - [Y]ank [I]nside [N]ext [']quote
+			--  - ci'  - [C]hange [I]nside [']quote
+			require("mini.ai").setup({ n_lines = 500 })
 
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+			-- Add/delete/replace surroundings (brackets, quotes, etc.)
+			--
+			-- - Saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+			-- - Sd'   - [S]urround [D]elete [']quotes
+			-- - Sr)'  - [S]urround [R]eplace [)] [']
+			require("mini.surround").setup({
+				mappings = {
+					add = "Sa", -- Add surrounding in Normal and Visual modes
+					delete = "Sd", -- Delete surrounding
+					find = "Sf", -- Find surrounding (to the right)
+					find_left = "SF", -- Find surrounding (to the left)
+					highlight = "Sh", -- Highlight surrounding
+					replace = "Sr", -- Replace surroundingmini
+					update_n_lines = "Sn", -- Update `n_lines`
+				},
+			})
+			-- Simple and easy statusline.
+			--  You could remove this setup call if you don't like it,
+			--  and try some other statusline plugin
+			local statusline = require("mini.statusline")
+			-- set use_icons to true if you have a Nerd Font
+			statusline.setup({ use_icons = vim.g.have_nerd_font })
 
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
+			-- You can configure sections in the statusline by overriding their
+			-- default behavior. For example, here we set the section for
+			-- cursor location to LINE:COLUMN
+			---@diagnostic disable-next-line: duplicate-set-field
+			statusline.section_location = function()
+				return "%2l:%-2v"
+			end
 
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
-
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
-    end,
-  },
+			-- ... and there is more!
+			--  Check out: https://github.com/echasnovski/mini.nvim
+		end,
+	},
 }
 -- vim: ts=2 sts=2 sw=2 et
